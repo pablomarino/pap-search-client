7636ea619702641bb15ee69b1d1faf447bac5c68
pablo <pablo.marino.boga@gmail.com>
2023-10-13 10:26:31 +0200

    Updates searchbox

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SearchboxComponent } from './searchbox/searchbox.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), SearchboxComponent],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
